adfd85b4040759f5929f1b145997ad72367661f5
gwarren3210 <98773623+gwarren3210@users.noreply.github.com>
2026-08-31 13:42:02 -0500

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        out = ''
+        for s in strs:
+            out += str(len(s))+':'+s
+        return out
+    def decode(self, s: str) -> List[str]:
+        out = []
+        i = j = 0
+        print(s)
+        while i<len(s):
+            i = s.find(':', j)
+            if i == -1: break
+            n = int(s[j:i])
+            out.append(s[i+1:i+n+1])
+            j = i+n+1
+        return out
